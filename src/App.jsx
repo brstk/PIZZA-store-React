9b9530d77639b98495c5affc39c96ffcee96830c
Tanya <9152204088zz@gmail.com>
2023-02-09 19:39:39 +0300

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 import Sort from './components/Sort';
 
 import './scss/app.scss';
@@ -9,10 +10,13 @@ import './scss/app.scss';
 export default function App() {
   const [pizzas, setPizzas] = React.useState([]);
 
+  const [isLoading, setIsLoading] = React.useState(true);
+
   React.useEffect(() => {
     fetch('https://63cec708d2e8c29a9bdeaf78.mockapi.io/items')
       .then((res) => res.json())
       .then((data) => setPizzas(data));
+    setIsLoading(false);
   }, []);
   return (
     <div className="wrapper">
@@ -26,9 +30,11 @@ export default function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {
-              pizzas.map((pizza) => (
-                <PizzaBlock pizza={pizza} key={pizza?.id} />
-              ))
+              isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+                : pizzas.map((pizza) => (
+                  <PizzaBlock pizza={pizza} key={pizza?.id} />
+                ))
             }
           </div>
         </div>
